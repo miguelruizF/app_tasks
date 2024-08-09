c610817f99953f422397524ee9056e7296b4f5f9
Miguel Ruiz <later481@gmail.com>
2024-08-08 21:38:57 -0600

    declaracion de estados Task_List component

--- src/components/container/Task_List.jsx
+++ src/components/container/Task_List.jsx
@@ -1,11 +1,34 @@
 // import React from 'react'
 
+import { useState } from "react";
 import { LEVELS } from "../../models/levels.enum";
 import { Task } from "../../models/task.class"
 import TaskComponent from "../pure/Task";
+import { useEffect } from "react";
 
 export const Task_ListComponent = () => {
+
     const defaultTask = new Task('Example', 'Default Description', false, LEVELS.NORMAL);
+    
+    //Estado que modificara las tareas. Estado del componente
+    const [tasks, setTasks] = useState([defaultTask]);
+
+    //true: cargara por defecto | false: para de cargar
+    const [loading, setLoading] = useState(true);
+
+    //Control del ciclo de vida del componente
+    useEffect(() => {
+        console.log('Task state has been modified');
+        setLoading(false);
+        return () => {
+            console.log('TaskList component is going to unmount...');
+        }
+    }, [tasks]);
+    
+
+    const changeCompleted = (id) =>{
+        console.log('TODO: Cambiar estado de una tarea');
+    }
 
     return (
         <div>
--- src/components/pure/Task.jsx
+++ src/components/pure/Task.jsx
@@ -1,15 +1,25 @@
 // import React from 'react'
 import PropTypes from 'prop-types'
 import { Task } from '../../models/task.class'
+import { useEffect } from 'react'
 
-const TaskComponent = ({task}) => {
+const TaskComponent = ({ task }) => {
+
+    useEffect(() => {
+        console.log('Created task');
+        return () => {
+            console.log(`Task: ${ task.name } is going to unmount...`);
+        }
+    }, [task]);
+    
+
     return (
         <div>
             <h2>Nombre: { task.name }</h2>
             <h3>Descripcion: { task.description }</h3>
             <h4>Nivel de la Tarea: { task.level }</h4>
             <h5>
-                Esta tarea esta: {task.completed ? "ESTA COMPLETADA" : "PENDIENTE"}
+                Esta tarea esta: { task.completed ? "ESTA COMPLETADA" : "PENDIENTE" }
             </h5>
         </div>
     )
