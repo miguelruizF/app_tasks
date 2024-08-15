928b7d71ecf4b8084d3270e6264b7c119e6bedb2
Miguel Ruiz <later481@gmail.com>
2024-08-14 23:00:10 -0600

    Ejemplos de useRef padre/hijo

--- src/components/container/Father.jsx
+++ src/components/container/Father.jsx
@@ -1,11 +1,24 @@
 // import React from 'react'
 
+import { useState } from "react";
 import { Child } from "../pure/Child"
 
 export const Father = () => {
+
+    const [name, setName] = useState('Mike');
+
+    function showMessage(text) {
+        alert(`Message receive: ${text}`);
+    }
+
+    function updateName(newName) {
+        setName( newName);
+    }
+
     return (
-        <div>
-            <Child/>
+        <div style={ { background: 'tomato', padding: '20px' } }>
+            {/* <Child name={ 'Martin' } send={ showMessage }/> */}
+            <Child name={ name } send={ showMessage } update={ updateName }/>
         </div>
-    )
+    );
 }
--- src/components/pure/Child.jsx
+++ src/components/pure/Child.jsx
@@ -1,18 +1,31 @@
 // import React from 'react'
 
-export const Child = ( {name} ) => {
+import { useRef } from "react";
+import { Button, Form, FormControl, FormGroup, FormText } from "react-bootstrap";
+
+export const Child = ( { name, send, update } ) => {
 
     function pressButton(){
-        alert( `Default Text` );
+        const text = messageRef.current.value //Acceder al valor del elemento
+        alert( `Text in input: ${text}` );
     }
 
     function pressButtonParams(text) {
         alert( `Text: ${text}` );
     }
 
+    //El useRef permite acceder a las referencias de un elemento
+    const messageRef = useRef('');
+    const nameRef = useRef('');
+
+    function submitName(e) {
+        e.preventDefault(); //Evitar la recarga
+        update( nameRef.current.value );
+    }
+
     return (
-        <div>
-            <p onMouseOver={ () => console.log('On Mouse Over') }> Child Component </p>
+        <div style={ { background: 'green' } }>
+            <p onMouseOver={ () => console.log('On Mouse Over') }> Hello { name } </p>
             <button onClick={ () => console.log('Boton 1 pulsado') }>
                 Boton 1
             </button>
@@ -22,6 +35,27 @@ export const Child = ( {name} ) => {
             <button onClick={ () => pressButtonParams('Hello') }>
                 Boton 3
             </button>
+            <input type="text" 
+            placeholder="Send a text to your father" 
+            onFocus={ () => console.log('Input Focused') } 
+            onChange={ (e) => console.log('Input Change:' , e.target.value) }
+            onCopy={ () => console.log('Copied text from input') }
+            ref={ messageRef } //Accedemos a la referencia del input
+            />
+            <button onClick={ () => send( messageRef.current.value ) }>Send Message</button>
+
+            <div style={ { margin: '20px', padding: "10px" } }>
+                {/* <form action="">
+                    <input type="text" placeholder="New Name" />
+                    <button type="submit">Update Name</button>
+                </form> */}
+                <Form onSubmit={ submitName }>
+                    <FormGroup>
+                        <FormControl type="text" placeholder="New Name" ref={ nameRef }></FormControl>
+                        <Button type="submit" style={ { marginTop: '10px' } }>Update Name</Button>
+                    </FormGroup>
+                </Form>
+            </div>
         </div>
     )
 }
