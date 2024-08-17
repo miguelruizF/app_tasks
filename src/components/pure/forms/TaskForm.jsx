2a0c09b4a723bcaa3cf9f7e9906f6ba92b5aed6f
Miguel Ruiz <later481@gmail.com>
2024-08-16 23:31:54 -0600

    Generacion de formulario - addTask

--- src/components/pure/forms/TaskForm.jsx
+++ src/components/pure/forms/TaskForm.jsx
@@ -1,11 +1,27 @@
 import { useRef } from "react"
+import { Form } from "react-bootstrap"
+import PropTypes from 'prop-types'
+import { LEVELS } from "../../../models/levels.enum";
 
+export const TaskForm = ( { add } ) => {
 
-export const TaskForm = () => {
+    //Referencias a los datos
+    const nameRed = useRef('');
+    const descriptionRef = useRef('');
+    const levelRef = useRef(LEVELS.NORMAL);
 
-    const first = useRef(second)
+    //Funcion para agregar tarea desde el formulario
+    function addTask(e) {
+        e.preventDefault(); //Evitar la recarga
+    }
 
     return (
-        <div>TaskForm</div>
+        <Form>
+
+        </Form>
     )
+}
+
+TaskForm.propTypes = {
+    add: PropTypes.func.isRequired
 }
