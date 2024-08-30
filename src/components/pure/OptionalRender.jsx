e7bda2f10e9da6b8e713cd2e24a91a6f442ea508
Miguel Ruiz <later481@gmail.com>
2024-08-29 22:19:13 -0600

    Ejemplo de render condicional ternario

--- src/components/pure/OptionalRender.jsx
+++ src/components/pure/OptionalRender.jsx
@@ -3,23 +3,44 @@
 import { useState } from "react"
 import { Button } from "react-bootstrap";
 
+let red = 0;
+let green = 200;
+let blue = 150;
+
+//Estilos si el usuario esta loggeado
+const loggedStyle = {
+    color: 'white',
+    backgroundColor: `rgb(${red}, ${green}, ${blue})`
+}
+
+//Estilos para usuario no loggeado
+const unloggedStyle = {
+    backgroundColor: 'tomato',
+    color: 'white',
+    fontWeight: 'bold'
+}
 
 //Login - Logout buttons
-const LoginButton = ( { loginAction } ) => {
+const LoginButton = ( { loginAction, propStyle } ) => {
     return (
-        <Button onClick={ loginAction }> Login </Button>
+        <Button style={ propStyle } onClick={ loginAction }> Login </Button>
     )
 }
 
-const LogoutButton = ( { logoutAction } ) => {
+const LogoutButton = ( { logoutAction, propStyle } ) => {
     return (
-        <Button onClick={ logoutAction }> Logout </Button>
+        <Button style={ propStyle } onClick={ logoutAction }> Logout </Button>
     )
 }
+
+
+// ? (Expresion true) && expresion => se renderiza la expresion
+// ? (Expresion false) && expresion => no se renderiza la expresion
 
 export const OptionalRender = () => {
 
-    const [access, setAccess] = useState( true );
+    const [access, setAccess] = useState( false );
+    const [message, setMessage] = useState(0);
 
     // const updateAccess = () => {
     //     setAccess(!access); //Se actualiza de true a false y viceversa
@@ -41,14 +62,34 @@ export const OptionalRender = () => {
     //     optionalButton = <button onClick={ updateAccess }> Login </button>
     // }
     if(access) {
-        optionalButton = <LogoutButton logoutAction={ logoutAction }></LogoutButton>
+        optionalButton = <LogoutButton propStyle={ unloggedStyle } logoutAction={ logoutAction }></LogoutButton>
     } else {
-        optionalButton = <LoginButton loginAction={ loginAction }></LoginButton>
+        optionalButton = <LoginButton propStyle={ loggedStyle } loginAction={ loginAction }></LoginButton>
+    }
+
+    //Unread messages
+    let addMessages = () => {
+        setMessage(message + 1);
     }
 
     return (
         <div>
+            {/* Optional Button */}
             { optionalButton }
+
+            {/* N Messages unread */}
+            {/*  { message > 0 && message === 1 && <p>You have one new message...</p> }
+            { message > 1 && <p>You have { message } new messages...</p> }
+            { message == 0 && <p>There are no new messages</p> } */}
+            
+            {/* Ternary Operator */}
+            {
+                access ? ( 
+                <div>
+                    { message > 0 ? <p>You have { message } new message{ message > 1 ? 's' : null }...</p> : <p>There are no new messages</p> }
+                    <Button onClick={ addMessages }> {message === 0 ? 'Add your first message' : 'Add New Message' }</Button>
+                </div> ) : null
+            }
         </div>
     )
 }
