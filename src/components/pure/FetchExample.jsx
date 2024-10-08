62b1c6da2c844fb0de287f666cfe701df05c3b48
Miguel Ruiz <later481@gmail.com>
2024-10-07 22:28:05 -0600

    use fetch - example completed

--- src/components/pure/FetchExample.jsx
+++ src/components/pure/FetchExample.jsx
@@ -1,10 +1,15 @@
 // import React from 'react'
 import { useEffect, useState } from "react"
-import { getAllUsers } from "../../services/fetchService"
+import { getAllPagedUsers, getAllUsers, getUsersDetails } from "../../services/fetchService"
+import { Height } from "@mui/icons-material";
 
 export const FetchExample = () => {
 
     const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState();
+    const [totalUsers, setTotalUsers] = useState(12);
+    const [usersPerPage, setUsersPerPage] = useState(6);
+    const [pages, setPages] = useState(2);
 
     useEffect(() => {
         obtainUsers();
@@ -22,6 +27,9 @@ export const FetchExample = () => {
                 } */
                     console.log('All users', response.data);
                     setUsers(response.data);
+                    setPages(response.total_pages);
+                    setUsersPerPage(response.per_page);
+                    setTotalUsers(response.total);
             })
             .catch((error) => alert(`Error while retreiving the users: ${error}`))
             .finally(() => {
@@ -29,15 +37,59 @@ export const FetchExample = () => {
                 console.table(users);
             })
     }
+
+    const obtainPageUsers = (page) => {
+        getAllPagedUsers(page)
+            .then((response) => {
+                console.log('All Page Users', response.data);
+                setUsers(response.data);
+                setPages(response.total_pages);
+                setUsersPerPage(response.per_page);
+                setTotalUsers(response.total);
+            })
+            .catch((error) => alert(`Error while retreiving the users: ${error}`))
+            .finally(() => {
+                console.log('Ended obtaining users');
+                console.table(users);
+            })
+    }
+
+    const obtainUserDetails = (id) => {
+        getUsersDetails(id)
+            .then((response) => {
+                console.log('All Page Users', response.data);
+                setSelectedUser(response.data);
+            })
+            .catch((error) => alert(`Error while retreiving the user: ${error}`))
+            .finally(() => {
+                console.log('Ended obtaining user');
+                console.table(selectedUser);
+            })
+    }
     
     return (
         <>
             <h2>Users: </h2>
             { users.map((user, index) =>
-                (<p key={ index }>
+                (<p key={ index } onClick={() => obtainUserDetails(user.id)}>
                     { user.first_name } { user.last_name }
                 </p>)
             ) }
+            <p>Showing {usersPerPage} users of {totalUsers} in total.</p>
+            <button onClick={() => obtainPageUsers(1)}>1</button>
+            <button onClick={() => obtainPageUsers(2)}>2</button>
+
+            <div>
+                <h3>User Details</h3>
+                {
+                    selectedUser && (<div>
+                        <p>Name: { selectedUser.first_name }</p>
+                        <p>Last Name: { selectedUser.last_name }</p>
+                        <p>Email: { selectedUser.email }</p>
+                        <img src={selectedUser.avatar} style={ {height: '50px', width:'50px'} } alt="Avatar" />
+                    </div>)
+                }
+            </div>
         </>
     )
 }
--- src/services/fetchService.js
+++ src/services/fetchService.js
@@ -9,3 +9,22 @@ export const getAllUsers = async () => {
     //we return the json
     return response.json();
 }
+
+
+export const getAllPagedUsers = async (page) => {
+    let response = await fetch(`https://reqres.in/api/users?page=${page}`);
+    console.log('Response: ', response);
+    console.log('Status', response.status);
+    console.log('OK?', response.ok);
+    //we return the json
+    return response.json();
+}
+
+export const getUsersDetails = async(id) => {
+    let response = await fetch(`https://reqres.in/api/users/${id}`);
+    console.log('Response: ', response);
+    console.log('Status', response.status);
+    console.log('OK?', response.ok);
+    //we return the json
+    return response.json();
+}
